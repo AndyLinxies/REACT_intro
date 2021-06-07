7fe12305a3b59a3cfc6e178c54f7558d5d88d492
AndyLinxies <linxies@gmail.com>
2021-06-07 16:07:04 +0200

    Intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  let dateCourante = new Date();
+  let heure = dateCourante.getTime();
+  console.log(dateCourante);
+  console.log(heure);
+  let mois;
+  if (dateCourante.getMonth()===5) {
+    mois ="Juin"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Temps JS :{heure} secondes se sont écoulés depuis 1970</h2>
+      <h2> Temps PHP : {heure/1000} secondes se sont écoulés depuis 1970</h2>
+      <h2>On est actuellement le : {dateCourante.getDate()} {mois} de l'année {dateCourante.getFullYear()} et il est : {dateCourante.getHours()} heures {dateCourante.getMinutes()} minutes</h2>
     </div>
   );
 }
